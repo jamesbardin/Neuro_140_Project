50a33da3b45e03d8278bb8db93fffa4ad9ff0848
jamesbardin <jbardin@college.harvard.edu>
2023-05-05 16:57:18 -0400

    after computer crash :( lost stuff in nb

--- Final/lib/dqn_model.py
+++ Final/lib/dqn_model.py
@@ -1,10 +1,7 @@
 import torch
 import torch.nn as nn
+
 import numpy as np
-import random
-import torch.optim as optim
-import torch.nn.functional as f
-from collections import deque
 
 
 class DQN(nn.Module):
--- Final/pong_run.py
+++ Final/pong_run.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from lib import wrappers
+from lib import wrappers_skips
 from lib import dqn_model
 import collections
 import argparse
@@ -15,10 +16,10 @@ import collections
 
 # envs = [wrappers_testing.make_env("PongNoFrameskip-v4", seed) for seed in range(1)]
 
-env = wrappers.make_env("PongNoFrameskip-v4")
+env = wrappers_skips.make_env("PongNoFrameskip-v4", 7)
 env.seed(42)
 net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
-net.load_state_dict(torch.load('C:/NEURO140/Final/PongNoFrameskip-v4-best.dat', map_location=lambda storage, loc: storage))
+net.load_state_dict(torch.load('C:/NEURO140/Final/PongNoFrameskip-v4-skip7.dat', map_location=lambda storage, loc: storage))
 
 state = env.reset()
 total_reward = 0.0
@@ -26,11 +27,10 @@ c = collections.Counter()
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('seed1.mp4', fourcc, 60.0, (env.render(mode='rgb_array').shape[1], env.render(mode='rgb_array').shape[0]))
+out = cv2.VideoWriter('seedlele5.mp4', fourcc, 60.0, (env.render(mode='rgb_array').shape[1], env.render(mode='rgb_array').shape[0]))
 
 done = False
 obs = env.reset()
-env.seed(41)
 while not done:
     action = net(torch.tensor([obs])).argmax(dim=1).item()
     obs, reward, done, _ = env.step(action)
